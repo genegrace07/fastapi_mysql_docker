347e398c7bd1ab962266a06e502db54d910c686b
mark anthony dador <dador.markanthony@gmail.com>
2026-01-29 21:53:51 +0800

    starting to build database

---
+++ appdb.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db = mysql.connector.connect(
+    host=os.getenv('dbhost'),
+    user=os.getenv('dbuser'),
+    password=os.getenv('dbpassword'),
+    database=os.getenv('dbdatabase')
+)
+
+##COLUMNS ON THIS TABLE
+# dbcursor.execute('create table users('
+#                  'id int auto_increment primary key,'
+#                  'name varchar(50) not null,'
+#                  'email varchar(100) not null )')
+##FIRST DATA ON USERS TABLE
+# dbcursor.execute('insert into users(name,email) '
+#                  'values("luffy","luffy@gmail.com"),'
+#                  '("zoro","zoro@gmail.com"),'
+#                  '("sanji","sanji@gmail.com")')
+
+class Users:
+    def view(self):
+        dbcursor = db.cursor(dictionary=True)
+        dbcursor.execute('select * from users')
+        result=dbcursor.fetchall()
+        print(result)
+
+
